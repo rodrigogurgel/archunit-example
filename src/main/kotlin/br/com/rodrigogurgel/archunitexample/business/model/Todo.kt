c23f46110c26b57907371d4ed48633b51da1eebd
rodrigogurgel <rodrigohenrique.gurgel@outlook.com>
2024-07-10 09:09:46 -0300

    remove rules from todo

--- src/main/kotlin/br/com/rodrigogurgel/archunitexample/business/model/Todo.kt
+++ src/main/kotlin/br/com/rodrigogurgel/archunitexample/business/model/Todo.kt
@@ -5,25 +5,8 @@ import java.util.UUID
 
 class Todo(
     val id: UUID,
-    description: String,
-    done: Boolean,
+    val description: String,
+    val done: Boolean,
     val createdAt: Instant,
-    updatedAt: Instant
-) {
-    var updatedAt: Instant = updatedAt
-        private set
-
-    var done: Boolean = done
-        set(value) {
-            this.updatedAt = Instant.now()
-            field = value
-        }
-
-    var description: String = description
-        set(value) {
-            field = value
-            check(description.isNotBlank()) { "TODO description cannot be blank" }
-            check(done) { "TODO done can't have description updated" }
-            updatedAt = Instant.now()
-        }
-}
+    val updatedAt: Instant
+)
